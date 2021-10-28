609445acb3d0d96ba16124858d8e9b4946737492
IvanC-20 <iejcaceres@gmail.com>
2021-10-28 04:16:21 -0300

    Tienda 0.1 login, cerrar sesion, mejoras en menu

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -94,4 +94,41 @@ class usuarioController{
        }
         header("Location:".base_url."usuario/registro");
     }
-}
+    
+    
+    public function login(){
+        if(isset($_POST)){
+            //identificar usuario
+            //consulta a la bd
+            $usuario = new Usuario();
+            $usuario->setEmail($_POST['email']);
+            $usuario->setPassword($_POST['password']);
+            $identity = $usuario->login();
+            
+           if($identity && is_object($identity)){
+               $_SESSION['identity'] = $identity;
+               
+               if($identity->rol == 'admin'){
+                   $_SESSION['admin'] = true;
+               }
+           }else{
+               $_SESSION['error_login'] = "Identificación fallida!";
+           }
+            //crear sesion
+        }
+        
+        header("Location:".base_url);
+    }
+    
+    public function logout(){
+        if(isset($_SESSION['identity'])){
+            unset($_SESSION['identity']);
+        }
+
+        if(isset($_SESSION['admin'])){
+            unset($_SESSION['admin']);
+        }
+        header("Location:".base_url);
+        }
+    
+} // fin clase
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,8 +1,9 @@
                <!-- Barra lateral -->
                 <aside id="lateral">
                     <div id="login" class="block-aside">
+                     <?php if(!isset($_SESSION['identity'])): ?>   
                         <h3>Entrar a la web</h3>
-                        <form action="#" method="POST"> 
+                        <form action="<?=base_url?>usuario/login" method="POST"> 
                             <label for="email">Email</label>
                             <input type="email" name="email"/>
 
@@ -10,12 +11,23 @@
                             <input type="password" name="password"/>
 
                             <input type="submit" value="Enviar"/>
-                        </form>    
-                    
+                        </form> 
+                     <?php else: ?>
+                        <h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>   
+                     <?php endif; ?>
                     <ul>
-                        <li><a href="#">Mis pedidos</a></li>
-                        <li><a href="#">Gestionar pedidos</a></li>
+                        
+                     <?php if(isset($_SESSION['admin'])): ?> 
                         <li><a href="#">Gestionar categorias</a></li>
+                        <li><a href="#">Gestionar productos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                     <?php endif; ?>   
+                        
+                     <?php if(isset($_SESSION['identity'])): ?>  
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="<?=base_url?>usuario/logout">Cerrar sesión</a></li>
+                     <?php endif; ?>   
+                    
                     </ul>
                     </div>    
                 </aside>
